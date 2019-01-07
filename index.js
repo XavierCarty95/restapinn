2e7bb3cb484d8a53144e4a3d5a90732d56849d48
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-01-07 23:25:54 +0000

    saving data to mongo

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const mongoose = require("mongoose")
 
+//set up express app 
 const app = express();
+
+//connect to mongodb
+
+mongoose.connect('mongodb://localhost/ninjago',{ useNewUrlParser: true });
+mongoose.Promise = global.Promise; 
+
 
 app.use(bodyParser.json());
 
+//intialize routes 
 app.use('/api', require('./routes/api'));
 
 // set up express app
--- routes/api.js
+++ routes/api.js
@@ -1,35 +1,27 @@
-const express = require("express")
+const express = require ('express');
 const router = express.Router();
-
+const Ninja = require('../ninja');
 
 // get a list of ninjas from the db
-router.get('/ninjas', function(req,res){
-   res.send({type: 'GET'});
-   
+router.get('/ninjas', function(req, res){
+    res.send({type: 'GET'});
 });
 
-//add a ninja in the db
-router.post('/ninjas', function(req,res){
-   console.log(req.body);
-   res.send(
-      {type: 'POST',
-      name: req.body.name,
-      rank: req.body.rank
-         
-      });
-   
+// add a new ninja to the db
+router.post('/ninjas', function(req, res){
+    Ninja.create(req.body).then(function(ninja){
+        res.send(ninja);
+    });
 });
 
 // update a ninja in the db
-router.delete('/ninjas:id', function(req,res){
-   res.send({type: 'DELETE'});
-   
+router.put('/ninjas/:id', function(req, res){
+    res.send({type: 'PUT'});
 });
 
 // delete a ninja from the db
-router.get('/ninjas', function(req,res){
-   res.send({type: 'GET'});
-   
+router.delete('/ninjas/:id', function(req, res){
+    res.send({type: 'DELETE'});
 });
 
-module.exports = router 
+module.exports = router;
