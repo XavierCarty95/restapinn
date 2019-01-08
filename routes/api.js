fb42ea9896b4af4d007aead58d0e4611a5c1087c
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-01-08 00:01:15 +0000

    handling delete request

--- routes/api.js
+++ routes/api.js
@@ -21,7 +21,10 @@ router.put('/ninjas/:id', function(req, res,next){
 
 // delete a ninja from the db
 router.delete('/ninjas/:id', function(req, res , next){
-    res.send({type: 'DELETE'});
+    Ninja.findByOneAndDelete({_id:req.params.id}).then(function(ninja){
+        res.send(ninja)
+    });
+    
 });
 
 module.exports = router;
